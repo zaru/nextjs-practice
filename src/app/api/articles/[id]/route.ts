20578b24913b173c9a6f8dda706e44aaa991484f
zaru <zarutofu@gmail.com>
2023-09-05 10:54:34 +0900

    SQliteをPrisma経由でアクセスするように修正

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,26 +1,17 @@
-import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
-
-let db: Database<sqlite3.Database, sqlite3.Statement> | null = null;
+import { prisma } from "@/lib/db/prisma";
 
 export async function PUT(
   request: Request,
   { params }: { params: { id: string } },
 ) {
-  if (!db) {
-    db = await open({
-      filename: "./sample.sqlite3",
-      driver: sqlite3.Database,
-    });
-  }
-
   const { id } = params;
   const { published } = await request.json();
-  console.log(`🎃 Request to PUT /api/articles/[${id}]`);
+  console.log(`🎃 Request to PUT /api/articles/[${id}]`, published);
 
-  await db.all(
-    `update articles set published = "${published}" where id = ${id}`,
-  );
+  await prisma.articles.update({
+    where: { id: Number(id) },
+    data: { published: published !== 0 },
+  });
 
   return new Response(JSON.stringify({ success: true }), {
     headers: { "Content-Type": "application/json" },
