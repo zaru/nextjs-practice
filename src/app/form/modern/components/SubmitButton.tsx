e36e03936d33e522b8372fd09bbf4afd99fcd0cf
zaru <zarutofu@gmail.com>
2024-02-15 22:32:57 +0900

    余計な属性を与えてエラーになっていたのを修正

--- src/app/form/modern/components/SubmitButton.tsx
+++ src/app/form/modern/components/SubmitButton.tsx
@@ -4,16 +4,16 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   clientSideInvalid?: boolean;
 }
 
-export function SubmitButton(props: Props) {
+export function SubmitButton({ clientSideInvalid, ...rest }: Props) {
   const { pending } = useFormStatus();
 
   return (
     <button
       type="submit"
-      disabled={pending || props.clientSideInvalid}
-      aria-disabled={pending || props.clientSideInvalid}
+      disabled={pending || clientSideInvalid}
+      aria-disabled={pending || clientSideInvalid}
       className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:opacity-30 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      {...props}
+      {...rest}
     >
       {pending ? "Submitting..." : "Submit"}
     </button>
