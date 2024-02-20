2d0989fb8c2ed46d609854882957326c36533e79
zaru <zarutofu@gmail.com>
2024-02-20 18:44:25 +0900

    Sentry設定テスト
    - ServerActionsはトラッキングOK
    - ServerComponentはNG

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7f9fe51e9cea738797132f64b83e3a66@o29253.ingest.sentry.io/4506774080782336",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
