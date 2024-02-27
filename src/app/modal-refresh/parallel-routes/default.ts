2e73203bb1680db737a1034907dae25a36dc326a
zaru <zarutofu@gmail.com>
2024-02-27 13:41:19 +0900

    default.tsxをdefault.tsにしてシンプルな書き方に変更

---
+++ src/app/modal-refresh/parallel-routes/default.ts
@@ -0,0 +1,7 @@
+export { default } from "./page";
+
+// tsxにして以下のように明示的にやってもOK
+// import Page from "./page";
+// export default function Default() {
+//   return <Page />;
+// };
--- src/app/modal-refresh/parallel-routes/default.tsx
+++
@@ -1,5 +0,0 @@
-import Page from "./page";
-
-export default function Default() {
-  return <Page />;
-}
